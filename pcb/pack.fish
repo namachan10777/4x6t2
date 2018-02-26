fb30ad640addb1b9ec8b4d682e8d1d592a3cd0c1
namachan10777 <namachan10777@gmail.com>
2018-02-26 23:51:50 +0900

    add packing script

---
+++ pcb/pack.fish
@@ -0,0 +1,23 @@
+#!/usr/bin/fish
+
+set basename $argv[1]
+
+if test $basename = 'left' -o $basename = 'right' -o $basename = 'sidebutton'
+	cd $basename
+	cp $basename-B.Cu.gbl      $basename.gbl
+	cp $basename-B.Mask.gbs    $basename.gbs
+	cp $basename-B.SilkS.gbo   $basename.gbo
+	cp $basename-Edge.Cuts.gm1 $basename.gm1
+	cp $basename-F.Cu.gtl      $basename.gtl
+	cp $basename-F.Mask.gts    $basename.gts
+	cp $basename-F.SilkS.gto   $basename.gto
+	cp $basename.drl           $basename.drl
+	set generated_files $basename.gbl $basename.gbs $basename.gbo $basename.gm1 $basename.gtl $basename.gts $basename.gto $basename.drl
+	zip $basename $generated_files
+	rm $generated_files
+
+else if test basename = '-h' -o basename = '--help'
+	echo 'fish pack.fish [left|right|sidebutton]'
+else
+	echo 'wrong pcb name'
+end
